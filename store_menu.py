e82737d0191eb504de0747eb62010af5adc0bb3d
EmberNine <33534064+EmberNine@users.noreply.github.com>
2020-07-10 21:03:06 -0500

    Create store_menu.py
    created a store menu template

---
+++ store_menu.py
@@ -0,0 +1,50 @@
+from tabulate import tabulate
+import os
+
+
+
+
+def menu():
+   
+    print("\n \n \n \n \n")
+    menutext = "Welcome to the Lego store"
+    table = [[menutext]]
+    output = tabulate(table, tablefmt='grid')
+    print(output)
+
+
+
+
+    print("1.) \n")
+
+    print("2.) \n")
+
+    print("3.) \n")
+
+    print("4.) \n")
+
+    print("5.) \n")
+    
+    choice = input()
+
+    if choice ==  "1":
+        print("You have 1 tussy \n")
+        menu()
+    
+    if choice ==  "2":
+        print("You have 2 tussy \n")
+        menu()
+
+    if choice ==  "3":
+        print("You have 3 tussy \n")
+        menu()
+
+    if choice ==  "4":
+        print("You have 4 tussy \n")
+        menu()
+
+    if choice ==  "5":
+        print("You have 5 tussy \n")
+        menu()
+
+menu()
