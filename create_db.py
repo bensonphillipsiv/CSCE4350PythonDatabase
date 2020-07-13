abe85b8efe38cdc0ebd944e6d35bbe6ceb57d817
Benson Phillips IV <benson.phillipsiv@gmail.com>
2020-07-12 22:58:39 -0500

    Fully Connected and Created Tables

--- create_db.py
+++ create_db.py
@@ -20,7 +20,7 @@ legodb = mysql.connector.connect(
 
 mycursor = legodb.cursor()
 
-# creating the customer table
+# creating the Customer table
 mycursor.execute(
     "CREATE TABLE Customers ("
     "customer_id INT, "
@@ -32,26 +32,30 @@ mycursor.execute(
     "PRIMARY KEY (customer_id))"
 )
 
-# creating the Orders Table
+#creating the Stores table
 mycursor.execute(
-    "CREATE TABLE Orders ("
-    "order_id INT, "
-    "customer_id INT, "
-    "employee_id INT, "
-    "payment_method VARCHAR(255), "
-    "delivery_date TIMESTAMP, "
-    "order_date TIMESTAMP, "
-    "PRIMARY KEY (order_id))"
+    "CREATE TABLE Stores (store_id INT, "
+    "store_name VARCHAR(255), "
+    "store_type int, "
+    "PRIMARY KEY (store_id))"
 )
 
-# creating the OrderItems Table
+#creating the Employee table
 mycursor.execute(
-    "CREATE TABLE OrderItems ("
-    "order_id INT, "
-    "brick_id INT, "
-    "brick_set_id INT, "
-    "brick_quantity INT, "
-    "brick_set_quantity INT)"
+    "CREATE TABLE Employees (employee_id INT, "
+    "employee_name VARCHAR(255), "
+    "employee_password VARCHAR(255), "
+    "store_id INT, "
+    "employee_type INT, "
+    "PRIMARY KEY (employee_id), "
+    "FOREIGN KEY (store_id) REFERENCES Stores(store_id))"
+)
+
+#creating the BrickSet table
+mycursor.execute(
+    "CREATE TABLE BrickSets (brick_set_id INT, "
+    "description VARCHAR(255), "
+    "PRIMARY KEY (brick_set_id))"
 )
 
 # creating the Bricks Table
@@ -62,35 +66,43 @@ mycursor.execute(
     "stock_quantity INT, "
     "brick_price INT, "
     "description VARCHAR(255), "
-    "PRIMARY KEY (brick_id))"
-#creating the Employee table
-mycursor.execute(
-    "CREATE TABLE Employees (employee_id INT, "
-    "employee_name VARCHAR(255), "
-    "employee_password VARCHAR(255), "
-    "store_id INT, "
-    "employee_type INT, "
-    "PRIMARY KEY (employee_id))"
+    "PRIMARY KEY (brick_id), "
+    "FOREIGN KEY (store_id) REFERENCES Stores(store_id))"
 )
 
-#creating the Stores table
+# creating the BrickSetItems table
 mycursor.execute(
-    "CREATE TABLE Stores (store_id INT, "
-    "store_name VARCHAR(255), "
-    "store_type int, "
-    "PRIMARY KEY (store_id))"
-)
-
-#creating the BrickSetItems table
-mycursor.execute(
-    "CREATE TABLE BrickSetItems (brick_set_id INT, "
+    "CREATE TABLE BrickSetItems ("
+    "brick_set_id INT, "
     "brick_id INT, "
-    "quanity INT)"
+    "quantity INT, "
+    "FOREIGN KEY (brick_set_id) REFERENCES BrickSets(brick_set_id))"
 )
 
-#creating the BrickSet table
+# creating the Orders Table
 mycursor.execute(
-    "CREATE TABLE BrickSet (brick_set_id INT, "
-    "description VARCHAR(255), "
-    "PRIMARY KEY (brick_set_id))"
+    "CREATE TABLE Orders ("
+    "order_id INT, "
+    "customer_id INT, "
+    "employee_id INT, "
+    "payment_method VARCHAR(255), "
+    "delivery_date TIMESTAMP, "
+    "order_date TIMESTAMP, "
+    "PRIMARY KEY (order_id), "
+    "FOREIGN KEY (customer_id) REFERENCES Customers(customer_id), "
+    "FOREIGN KEY (employee_id) REFERENCES Employees(employee_id))"
+)
+
+# creating the OrderItems Table
+mycursor.execute(
+    "CREATE TABLE OrderItems ("
+    "order_id INT, "
+    "brick_id INT, "
+    "brick_set_id INT, "
+    "brick_quantity INT, "
+    "brick_set_quantity INT, "
+    "FOREIGN KEY (order_id) REFERENCES Orders(order_id), "
+    "FOREIGN KEY (brick_id) REFERENCES Bricks(brick_id), "
+    "FOREIGN KEY (brick_set_id) REFERENCES BrickSets(brick_set_id))"
+
 )
